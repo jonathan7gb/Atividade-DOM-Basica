2acd1d001b7cb89e961581cc0ed7b0c106a8e4b7
jonathan_uber <jonathanuber121207@gmail.com>
2025-10-31 18:59:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const nome_input = document.getElementById("nome-input")
+const cartao_nome = document.getElementById("cartao-nome")
+
+
+nome_input.addEventListener('input', function(){
+    cartao_nome.innerText = nome_input.value
+})
+
+// =========================================
+
+const cartao = document.getElementById("cartao")
+const btnAzul = document.getElementById("btn-azul")
+const btnVerde= document.getElementById("btn-verde")
+
+btnAzul.addEventListener('click', () => {
+    cartao.classList.remove('fundo-verde')
+    cartao.classList.add('fundo-azul')
+})
+
+btnVerde.addEventListener('click', () => {
+    cartao.classList.remove('fundo-azul')
+    cartao.classList.add('fundo-verde')
+})
+
+// =========================================
+
+const mudar_fonte = document.getElementById("btn-fonte")
+
+mudar_fonte.addEventListener('click', () => {
+    mudar_fonte.classList.toggle('fonte-alternativa')
+})
+
+// =========================================
+
+const select_box = document.getElementById("imagem-select")
+const imagem = document.getElementById("cartao-img")
+
+select_box.addEventListener('change', () => {
+    imagem.setAttribute('src', select_box.value)
+})
